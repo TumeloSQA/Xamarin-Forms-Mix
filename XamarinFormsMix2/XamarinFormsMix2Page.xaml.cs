8168d5873c29d1c78b767d5b486ab583e23b9202
Tumelo <tumelophuti@gmail.com>
2017-07-05 15:31:50 +0200

    First Commit

---
+++ XamarinFormsMix2/XamarinFormsMix2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsMix2
+{
+    public partial class XamarinFormsMix2Page : ContentPage
+    {
+        public XamarinFormsMix2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
